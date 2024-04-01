322c3182444756d3fe82d4824179b05320e09ea0
Sam Briggs <git@sambriggs.dev>
2024-04-02 02:58:58 +0300

    Add loading spinner

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,14 +1,37 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { TextInput, Textarea, Button } from '@svelteuidev/core';
+	import { TextInput, Textarea, Button, Loader } from '@svelteuidev/core';
+
+	enum SearchState {
+		Initial = 1,
+		Searching,
+		Searched
+	}
 
 	export let form = null;
 	$: responsePayload = JSON.stringify(form);
+
+	let searchState = SearchState.Initial;
 </script>
 
-<form method="POST" use:enhance>
+<form
+	method="POST"
+	use:enhance={({}) => {
+		searchState = SearchState.Searching;
+		return async ({ update }) => {
+			searchState = SearchState.Searched;
+			update();
+		};
+	}}
+>
 	<TextInput name="indexName" placeholder="Index name" label="Index name" required />
 	<Textarea name="searchBody" placeholder="Search API body" label="Query" required />
 	<Button type="submit">Search</Button>
 </form>
-<Textarea disabled label="Query results" bind:value={responsePayload} />
+<Textarea disabled label="Query results" bind:value={responsePayload}>
+	<svelte:fragment slot="rightSection">
+		{#if searchState == SearchState.Searching}
+			<Loader color="blue" size="xs" />
+		{/if}
+	</svelte:fragment>
+</Textarea>
