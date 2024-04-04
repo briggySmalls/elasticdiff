a899de8946aa3756b01023935e818258fa68635f
Sam Briggs <git@sambriggs.dev>
2024-04-04 08:10:27 +0100

    Use table styles

--- src/routes/search/SearchResults.svelte
+++ src/routes/search/SearchResults.svelte
@@ -55,46 +55,48 @@
 		</svelte:fragment>
 	</AccordionItem>
 </Accordion>
-<table>
-	<thead>
-		{#each $table.getHeaderGroups() as headerGroup}
-			<tr>
-				{#each headerGroup.headers as header}
-					<th>
-						{#if !header.isPlaceholder}
-							<svelte:component
-								this={flexRender(header.column.columnDef.header, header.getContext())}
-							/>
-						{/if}
-					</th>
-				{/each}
-			</tr>
-		{/each}
-	</thead>
-	<tbody>
-		{#each $table.getRowModel().rows as row}
-			<tr>
-				{#each row.getVisibleCells() as cell}
-					<td>
-						<svelte:component this={flexRender(cell.column.columnDef.cell, cell.getContext())} />
-					</td>
-				{/each}
-			</tr>
-		{/each}
-	</tbody>
-	<tfoot>
-		{#each $table.getFooterGroups() as footerGroup}
-			<tr>
-				{#each footerGroup.headers as header}
-					<th>
-						{#if !header.isPlaceholder}
-							<svelte:component
-								this={flexRender(header.column.columnDef.footer, header.getContext())}
-							/>
-						{/if}
-					</th>
-				{/each}
-			</tr>
-		{/each}
-	</tfoot>
-</table>
+<div class="table-container">
+	<table class="table table-hover">
+		<thead>
+			{#each $table.getHeaderGroups() as headerGroup}
+				<tr>
+					{#each headerGroup.headers as header}
+						<th>
+							{#if !header.isPlaceholder}
+								<svelte:component
+									this={flexRender(header.column.columnDef.header, header.getContext())}
+								/>
+							{/if}
+						</th>
+					{/each}
+				</tr>
+			{/each}
+		</thead>
+		<tbody>
+			{#each $table.getRowModel().rows as row}
+				<tr>
+					{#each row.getVisibleCells() as cell}
+						<td>
+							<svelte:component this={flexRender(cell.column.columnDef.cell, cell.getContext())} />
+						</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+		<tfoot>
+			{#each $table.getFooterGroups() as footerGroup}
+				<tr>
+					{#each footerGroup.headers as header}
+						<th>
+							{#if !header.isPlaceholder}
+								<svelte:component
+									this={flexRender(header.column.columnDef.footer, header.getContext())}
+								/>
+							{/if}
+						</th>
+					{/each}
+				</tr>
+			{/each}
+		</tfoot>
+	</table>
+</div>
